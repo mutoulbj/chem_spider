77f55481d8be0e8bea0c368b6b5c1370c58b134a
mutoulbj <flt0426@163.com>
2015-04-01 03:23:58 +0800

    add try except

--- chem_spider/sigma_product_detail.py
+++ chem_spider/sigma_product_detail.py
@@ -35,158 +35,161 @@ class ProductDetail(object):
         }
 
     def get_base_info(self, url):
-        res = self.get_res(url)
-        if res:
-            d = {}
-            p = pq(res.text)
-            pd = p("div#productDetailHero div.contentStage div.productInfo")
+        try:
+            res = self.get_res(url)
+            if res:
+                d = {}
+                p = pq(res.text)
+                pd = p("div#productDetailHero div.contentStage div.productInfo")
 
-            strongs = pq(pd).find("strong")
-            for st in strongs:
-                if u"产品编号" in pq(st).text():
-                    d["number"] = pq(st).text().replace(u"产品编号","").replace("|", "").strip()
-                elif u"CAS号" in pq(st).text():
-                    d["cas"] = pq(st).text().replace(u"CAS号","").replace("|", "").strip()
-                elif pq(st).attr("itemprop") == "brand":
-                    d["brand"] = pq(st).text().strip()
+                strongs = pq(pd).find("strong")
+                for st in strongs:
+                    if u"产品编号" in pq(st).text():
+                        d["number"] = pq(st).text().replace(u"产品编号","").replace("|", "").strip()
+                    elif u"CAS号" in pq(st).text():
+                        d["cas"] = pq(st).text().replace(u"CAS号","").replace("|", "").strip()
+                    elif pq(st).attr("itemprop") == "brand":
+                        d["brand"] = pq(st).text().strip()
 
-            h1s = pq(pd).find("h1")
-            for h1 in h1s:
-                if pq(h1).attr("itemprop") == "name":
-                    d["name"] = pq(h1).text().strip()
+                h1s = pq(pd).find("h1")
+                for h1 in h1s:
+                    if pq(h1).attr("itemprop") == "name":
+                        d["name"] = pq(h1).text().strip()
 
-            h2s = pq(pd).find("h2")
-            for h2 in h2s:
-                if pq(h2).attr("class") == "english_subtitle":
-                    d["en_name"] = pq(h2).text().strip()
-                elif pq(h2).attr("itemprop") == "description":
-                    d["pure"] = pq(h2).text().strip()  # 纯度
+                h2s = pq(pd).find("h2")
+                for h2 in h2s:
+                    if pq(h2).attr("class") == "english_subtitle":
+                        d["en_name"] = pq(h2).text().strip()
+                    elif pq(h2).attr("itemprop") == "description":
+                        d["pure"] = pq(h2).text().strip()  # 纯度
 
-            ps = pq(pd).find("p")
-            for x in ps:
-                if u"别名" in pq(x).text():
-                    d["alias_name"] = pq(x).text().replace(u"别名:", "").strip()
+                ps = pq(pd).find("p")
+                for x in ps:
+                    if u"别名" in pq(x).text():
+                        d["alias_name"] = pq(x).text().replace(u"别名:", "").strip()
 
-            lis = pq(pd)("ul").find("li")
-            if lis:
-                d["base_other"] = {}
-                for li in lis:
-                    if u"分子式" in pq(li).text():
-                        d["formula"] = pq(li).text().strip().split(" ")[1]
-                    elif u"分子量" in pq(li).text():
-                        d["weight"] = pq(li).text().strip().split(" ")[-1]
-                    elif "MDL" in pq(li).text():
-                        d["mdl"] = pq(li).text().strip().split(" ")[-1]
-                    elif "Registry Number" in pq(li).text():
-                        d["registry_number"] = pq(li).text().strip().split(" ")[-1]
-                    elif "EINECS" in pq(li).text():
-                        d["einecs"] = pq(li).text().strip().split(" ")[-1]
-                    elif "Substance" in pq(li).text():
-                        d["substance"] = pq(li).text().strip().split(" ")[-1]
-                    else:
-                        l = pq(li).text().strip().split(" ")
-                        k = "_".join(l[0: -1]).strip().replace('.', '-')
-                        d["base_other"][k] = l[-1]
+                lis = pq(pd)("ul").find("li")
+                if lis:
+                    d["base_other"] = {}
+                    for li in lis:
+                        if u"分子式" in pq(li).text():
+                            d["formula"] = pq(li).text().strip().split(" ")[1]
+                        elif u"分子量" in pq(li).text():
+                            d["weight"] = pq(li).text().strip().split(" ")[-1]
+                        elif "MDL" in pq(li).text():
+                            d["mdl"] = pq(li).text().strip().split(" ")[-1]
+                        elif "Registry Number" in pq(li).text():
+                            d["registry_number"] = pq(li).text().strip().split(" ")[-1]
+                        elif "EINECS" in pq(li).text():
+                            d["einecs"] = pq(li).text().strip().split(" ")[-1]
+                        elif "Substance" in pq(li).text():
+                            d["substance"] = pq(li).text().strip().split(" ")[-1]
+                        else:
+                            l = pq(li).text().strip().split(" ")
+                            k = "_".join(l[0: -1]).strip().replace('.', '-')
+                            d["base_other"][k] = l[-1]
 
-            # 性质
-            prop = p("div#productDetailProperties table")
-            if prop:
-                trs = pq(prop).find("tr")
-                d["prop_other"] = {}
-                for tr in trs:
-                    td0 = pq(pq(tr)("td").eq(0)).text().strip()
-                    td1 = pq(pq(tr)("td").eq(1)).text().replace(u"更多...", "").strip()
-                    if "Related Categories" in td0:
-                        d["related_cate"] = td1
-                    elif "vapor pressure" in td0:
-                        d["vapor"] = td1
-                    elif "assay" in td0:
-                        d["assay"] = td1
-                    elif "autoignition temp" in td0:
-                        d["autoignition_temp"] = td1
-                    elif "bp" in td0:
-                        d["bp"] = td1
-                    elif "mp" in td0:
-                        d["mp"] = td1
-                    elif "density" in td0:
-                        d["density"] = td1
-                    else:
-                        k = td0.strip().replace(" ", "_").replace('.', '-')
-                        d["prop_other"][k] = td1
-
-            # 安全信息
-            safety = p("div#productDetailSafety table")
-            if safety:
-                trs = pq(prop).find("tr")[1:]
-                d["safety_other"] = {}
-                for tr in trs:
-                    td0 = pq(pq(tr)("td").eq(0)).text().strip()
-                    td1 = pq(pq(tr)("td").eq(1)).text().strip()
-                    if u"信号词" in td0:
-                        d["sign_word"] = td1  # 信号词
-                    elif u"危险声明" in td0:
-                        d["danger_say"] = td1  # 危险声明
-                    else:
-                        k = td0.strip().replace(' ', '_').replace('.', '-')
-                        d["safety_other"][k] = td1
-
-            # 产品描述
-            desc = p("div#productDescription div.descriptionContent")
-            if desc:
-                d["desc"] = pq(desc).text().strip()
-
-            # 保存基本信息
-            self.db_detail.update({"number": d["number"]}, {"$set": d}, upsert=True)
-
-            # 库存与价格
-            p_url = "http://www.sigmaaldrich.com/catalog/PricingAvailability.do?"
-            payload = {
-                "productNumber": d.get("number", ""),
-                "brandKey": d.get("brand", ""),
-                "divId": "pricingContainerMessage"
-            }
-            r_url = p_url+urllib.urlencode(payload)
-            r = self.post_res(r_url)
-            if r:
-                d_price = {
-                    "number": d.get("number", ""),
-                    "cas": d.get("cas", ""),
-                    "name": d.get("name" ""),
-                    "en_name": d.get("en_name", ""),
-                    "pure": d.get("pure", "")
-                }
-                s = r.content
-                try:
-                    index = r.text.index("clearfix")
-                    if s[index+9] == ' ':
-                        l = list(s)
-                        l[index + 9] = ">"
-                        c = pq("".join(l))
-                    else:
-                        c = pq(s)
-                except ValueError, e:
-                    c = pq(s)
-
-                pro_detail_inner = c("div.product-details-outer div.product-details-inner")
-                message = pq(pro_detail_inner)("div.product-discontinued").text().strip()
-                trs = pq(pro_detail_inner)("table").find("tr")[1:]
-                if trs:
+                # 性质
+                prop = p("div#productDetailProperties table")
+                if prop:
+                    trs = pq(prop).find("tr")
+                    d["prop_other"] = {}
                     for tr in trs:
-                        td_0 = pq(tr)("td").eq(0)
-                        td_1 = pq(tr)("td").eq(1)
-                        td_2 = pq(tr)("td").eq(3)
-                        spec = pq(td_0).text().strip()
-                        shipping = pq(td_1).text().strip()
-                        price = pq(td_2).text().strip()
+                        td0 = pq(pq(tr)("td").eq(0)).text().strip()
+                        td1 = pq(pq(tr)("td").eq(1)).text().replace(u"更多...", "").strip()
+                        if "Related Categories" in td0:
+                            d["related_cate"] = td1
+                        elif "vapor pressure" in td0:
+                            d["vapor"] = td1
+                        elif "assay" in td0:
+                            d["assay"] = td1
+                        elif "autoignition temp" in td0:
+                            d["autoignition_temp"] = td1
+                        elif "bp" in td0:
+                            d["bp"] = td1
+                        elif "mp" in td0:
+                            d["mp"] = td1
+                        elif "density" in td0:
+                            d["density"] = td1
+                        else:
+                            k = td0.strip().replace(" ", "_").replace('.', '-')
+                            d["prop_other"][k] = td1
+
+                # 安全信息
+                safety = p("div#productDetailSafety table")
+                if safety:
+                    trs = pq(prop).find("tr")[1:]
+                    d["safety_other"] = {}
+                    for tr in trs:
+                        td0 = pq(pq(tr)("td").eq(0)).text().strip()
+                        td1 = pq(pq(tr)("td").eq(1)).text().strip()
+                        if u"信号词" in td0:
+                            d["sign_word"] = td1  # 信号词
+                        elif u"危险声明" in td0:
+                            d["danger_say"] = td1  # 危险声明
+                        else:
+                            k = td0.strip().replace(' ', '_').replace('.', '-')
+                            d["safety_other"][k] = td1
+
+                # 产品描述
+                desc = p("div#productDescription div.descriptionContent")
+                if desc:
+                    d["desc"] = pq(desc).text().strip()
+
+                # 保存基本信息
+                self.db_detail.update({"number": d["number"]}, {"$set": d}, upsert=True)
+
+                # 库存与价格
+                p_url = "http://www.sigmaaldrich.com/catalog/PricingAvailability.do?"
+                payload = {
+                    "productNumber": d.get("number", ""),
+                    "brandKey": d.get("brand", ""),
+                    "divId": "pricingContainerMessage"
+                }
+                r_url = p_url+urllib.urlencode(payload)
+                r = self.post_res(r_url)
+                if r:
+                    d_price = {
+                        "number": d.get("number", ""),
+                        "cas": d.get("cas", ""),
+                        "name": d.get("name" ""),
+                        "en_name": d.get("en_name", ""),
+                        "pure": d.get("pure", "")
+                    }
+                    s = r.content
+                    try:
+                        index = r.text.index("clearfix")
+                        if s[index+9] == ' ':
+                            l = list(s)
+                            l[index + 9] = ">"
+                            c = pq("".join(l))
+                        else:
+                            c = pq(s)
+                    except ValueError, e:
+                        c = pq(s)
+
+                    pro_detail_inner = c("div.product-details-outer div.product-details-inner")
+                    message = pq(pro_detail_inner)("div.product-discontinued").text().strip()
+                    trs = pq(pro_detail_inner)("table").find("tr")[1:]
+                    if trs:
+                        for tr in trs:
+                            td_0 = pq(tr)("td").eq(0)
+                            td_1 = pq(tr)("td").eq(1)
+                            td_2 = pq(tr)("td").eq(3)
+                            spec = pq(td_0).text().strip()
+                            shipping = pq(td_1).text().strip()
+                            price = pq(td_2).text().strip()
+                            d_copy = copy.deepcopy(d_price)
+                            d_copy["spec"] = spec
+                            d_copy["shipping"] = shipping
+                            d_copy["price"] = price
+                            self.db_price.update({"number": d_copy["number"]}, {"$set": d_copy}, upsert=True)
+                    if message:
                         d_copy = copy.deepcopy(d_price)
-                        d_copy["spec"] = spec
-                        d_copy["shipping"] = shipping
-                        d_copy["price"] = price
+                        d_copy["spec"] = message
                         self.db_price.update({"number": d_copy["number"]}, {"$set": d_copy}, upsert=True)
-                if message:
-                    d_copy = copy.deepcopy(d_price)
-                    d_copy["spec"] = message
-                    self.db_price.update({"number": d_copy["number"]}, {"$set": d_copy}, upsert=True)
+        except:
+            pass
 
 
 
