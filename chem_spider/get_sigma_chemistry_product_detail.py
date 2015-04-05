1323b1716883c766464a2c7387a79ec1c817f8a2
mutoulbj <flt0426@163.com>
2015-04-05 18:37:57 +0800

    update

--- chem_spider/get_sigma_chemistry_product_detail.py
+++ chem_spider/get_sigma_chemistry_product_detail.py
@@ -6,7 +6,7 @@ from sigma_product_detail import ProductDetail
 
 p = ProductDetail(db_price=db.chemistry_product_price, db_detail=db.chemistry_product_detail)
 
-pros = db.sigma_chemistry_product_urls.find(timeout=False)
+pros = db.sigma_chemistry_product_urls.find(timeout=False).skip(26887)
 for pro in pros:
     p.get_base_info(url=pro["url"])
 conn.close()
--- chem_spider/get_sigma_materials_product_detail.py
+++ chem_spider/get_sigma_materials_product_detail.py
@@ -6,7 +6,7 @@ from sigma_product_detail import ProductDetail
 
 p = ProductDetail(db_price=db.materials_product_price, db_detail=db.materials_product_detail)
 
-pros = db.sigma_materials_product_urls.find(timeout=False)
+pros = db.sigma_materials_product_urls.find(timeout=False).skip(26519)
 for pro in pros:
     p.get_base_info(url=pro["url"])
 conn.close()
