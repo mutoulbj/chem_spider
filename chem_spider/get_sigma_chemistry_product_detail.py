edc650e8bd8b760c4df8c40cb54ed4519d0fe007
mutoulbj <flt0426@163.com>
2015-04-07 16:01:01 +0800

    update skip count

--- chem_spider/get_sigma_chemistry_product_detail.py
+++ chem_spider/get_sigma_chemistry_product_detail.py
@@ -6,7 +6,8 @@ from sigma_product_detail import ProductDetail
 
 p = ProductDetail(db_price=db.chemistry_product_price, db_detail=db.chemistry_product_detail)
 
-pros = db.sigma_chemistry_product_urls.find(timeout=False).skip(26887)
+count = db.chemistry_product_detail.count()
+pros = db.sigma_chemistry_product_urls.find(timeout=False).skip(count)
 for pro in pros:
     p.get_base_info(url=pro["url"])
 conn.close()
